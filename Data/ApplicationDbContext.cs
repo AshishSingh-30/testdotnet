f5a1c61536804b18ee3e335305da00ac8494d496
Ashish Singh <108607@ROBINHOODINSURANCE.IN>
2024-10-10 12:38:34 +0530

    first commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using MyFirstBackend.Models;
+
+namespace MyFirstBackend.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
